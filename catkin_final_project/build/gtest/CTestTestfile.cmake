55ee1c694e73bd0ee78e8f14db83ff85dc232faa
sean94011 <sean94011@gmail.com>
2021-10-16 09:29:43 -0700

    set up new workspace on VM

---
+++ catkin_final_project/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gmock
+# Build directory: /home/ur3/ECE470_Project/catkin_final_project/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
